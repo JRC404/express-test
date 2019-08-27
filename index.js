63f9f6b83ae5232d740edb1eb453394c899222fb
Jacob Reilly-Cooper <jacob.reilly-cooper@wearecodenation.com>
2019-08-27 17:11:20 +0100

    First commit of express test, walkthrough with Dean

---
+++ index.js
@@ -0,0 +1,51 @@
+// either app.js or index.js - preferrence to call it index.js
+// config file for express
+// -y is yes, thanks Dean.
+
+// npm i express - because we are using express
+// body-parses - allows us to access form data
+// express-handlebars - allows us to use handlebars - templating
+// path - join paths, doesn't have to identify the path. It allows us to join paths.
+const hbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const express = require('express');
+// initialise express by calling it like a function. Allowing us to use epxress. Remember to hover over express.
+const app = express();
+
+const router = require('./routes/route')
+
+// config - capital letters. 
+// either use the port enviornment OR if it's local, it's 3000.
+// two ways...
+// const PORT = process.env.PORT || 3000;
+
+// this is preffered.
+const {PORT = 3000} = process.env;
+
+// express needs a view enginge... pain in the arse. 
+// look into specific hbs documentation - objects inside hbs function
+app.engine('.hbs', hbs({defaultLayout: 'layout', extname: '.hbs'}))
+
+// why do we have to set up .hbs?
+app.set('view engine', '.hbs');
+
+// multiple app.use
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyParser - urlencoded - read about it. body-parser
+// app.use is a uniqute method for express... look into that.
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use('/', router);
+
+
+
+
+// last thing in the file...
+// waiting for something to happen. Waiting on PORT.
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+});
+
---
+++ routes/route.js
@@ -0,0 +1,42 @@
+// requiring Router module from express
+const {Router} = require('express');
+const router = Router();
+
+router.get('/login', (req, res) => {
+    // let resource = '<h1>Hello, Dean.</h1>';
+    // // req - data they've sent you... client / browser
+    // // res - send information back to the browser
+    // // any type of data with res.send() hover for more info
+    // // only use res once. Can use in if statement.
+    // // ace if statement.
+    // // https://www.restapitutorial.com/httpstatuscodes.html
+    // if (resource != undefined) {
+    //     res.send(resource)
+    // }   else {
+    //     res.status(404).send('Idiot.');
+    // }
+
+    res.render('login', {title: "Express"})
+
+    
+});
+
+router.post('/login', (req, res) => {
+    let name = req.body.name;
+
+    // always have to send a response back, otherwise it will hang... Jacob, research...
+    res.render('login', {name, title: 'express'});
+}) 
+
+// router.get('*', (req, res) => {
+//     res.redirect('/login')
+// })
+
+router.get('/', (req, res) => {
+    // the data we are sending over. With a title of express.
+    res.render('index', {title: 'Express'});
+})
+
+
+
+module.exports = router;
